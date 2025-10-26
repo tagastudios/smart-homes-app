7d6373135ef0c5b30b06243a2e2a8cf1e28f8435
Jose Tannous <tagastudios@gmail.com>
2025-10-25 22:05:20 -0400

    init: firebase and guidelines added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,36 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxt/ui", "@nuxt/eslint", "@vite-pwa/nuxt"],
+  modules: [
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/eslint",
+    "@vite-pwa/nuxt",
+    "nuxt-vuefire",
+  ],
   css: ["~/assets/css/main.css"],
+  vuefire: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    },
+    auth: {
+      enabled: true,
+    },
+    firestore: {
+      enabled: true,
+    },
+    storage: {
+      enabled: true,
+    },
+    functions: {
+      enabled: true,
+    },
+  },
   pwa: {
     /* PWA options */
     manifest: {
